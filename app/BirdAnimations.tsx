e8bbc9e3cdc2e75c5ce0ba49e7501c3c206cdffc
murta21 <murtasakha@gmail.com>
2025-08-05 22:50:29 -0700

    animations fixes

--- app/BirdAnimations.tsx
+++ app/BirdAnimations.tsx
@@ -13,8 +13,8 @@ const BirdAnimations = () => {
     // We use CSS variables to give each bird a unique animation delay,
     // duration, and vertical position. This makes the animation feel more natural.
     const birdStyle = {
-      '--animation-delay': `${2 + Math.random() * 10}s`,
-      '--animation-duration': `${33 + Math.random() * 15}s`, // Fly across in 15-33 seconds
+      '--animation-delay': `${2 + Math.random() * 5}s`,
+      '--animation-duration': `${ 25 + Math.random() * 15}s`, // Fly across in 15-33 seconds
       top: `${11 + Math.random() * 48}%`, // Position birds in the top 10% to 50% of the screen
     } as React.CSSProperties;
 
